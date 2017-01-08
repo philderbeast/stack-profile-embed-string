21e78d06907922503ddeed0fa7e9fc5b63646608
Phil de Joux <phil.dejoux@blockscope.com>
2017-01-09 02:45:45 +1300

    Add embed string module and raw strings qq.

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import Lib
+import EmbedString (embedStr)
+import Text.RawString.QQ (r)
+
+license :: String
+license = intro ++ lic ++ newline
+    where
+        lic= $(embedStr (readFile "./LICENSE"))
+        newline = [r|
+|]
+        intro = [r|
+Included Software License
+-------------------------
+The LICENSE for this app is ...
+|]
+
 
 main :: IO ()
-main = someFunc
+main = putStr license
